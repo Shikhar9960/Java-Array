c9066ff1a403cd7f136aa0b1def61aae706c0bc4
Shikhar Jaiswal <144546090+Shikhar9960@users.noreply.github.com>
2025-01-28 15:07:54 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l;
+        System.out.println("enter the size of array");
+        l = sc.nextInt();
+        int arr[] = new int[l];
+        System.out.println("enter the elements in array");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        BubbleSort(arr, l);
+
+    }
+
+    public static void BubbleSort(int[] arr, int n) {
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+        System.out.println("sorted array");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k]);
+        }
+    }
+}
